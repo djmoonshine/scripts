1596b04490f2b9cadc454b4705a5604f55adb184
root <root@cp1.localdomain>
2016-07-05 15:29:09 +0200

    More changes

--- centos-initialsetup.sh
+++ centos-initialsetup.sh
@@ -1,10 +1,13 @@
-yum install wget
-yum install epel-release
-yum install bash-completion
-yum install iotop
-yum install sysstat
-yum install gdisk
-yum install nano
+nmtui
+yum -y install wget
+yum -y install epel-release
+yum -y install bash-completion
+yum -y install iotop
+yum -y install sysstat
+yum -y install gdisk
+yum -y install nano
+yum -y install fio
+yum -y install atop
 rpm --import 'https://download.ceph.com/keys/release.asc'
 wget http://ftp.hp.com/pub/softlib2/software1/pubsw-linux/p1944765023/v114614/hpssa-2.40-13.0.x86_64.rpm
 wget http://ftp.hp.com/pub/softlib2/software1/pubsw-linux/p1580676007/v112777/hpsmh-7.5.4-3.x86_64.rpm
